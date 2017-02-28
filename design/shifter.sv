47df253b1a22a0a5b394bc6d88c3c6426985edb2
Raymond Wang <raymond@insellerate.com>
2017-02-28 12:46:28 -0800

    Heyang's new shifter

--- design/shifter.sv
+++ design/shifter.sv
@@ -1,16 +1,25 @@
 module shifter(
     input logic [25:0] Instr,
 	input logic [31:0] Rm,
-	output logic [31:0] Rd
+	input logic [31:0] Rs,
+	output logic [31:0] Rd,
+	output logic [3:0] ShifterFlags
 );
     Rd = 32'b0;
+	automatic logic overflow = 1'b0;
+	automatic logic c_out = 1'b0;
+	automatic logic negative = 1'b0;
+	automatic logic zero = 1'b0;
 
-    if (Instr[25])
+    if (Instr[25] || ~Instr[11:4])
 	    Rd = Rm;
+		zero = ~|Rd;
+		negative = Rd[31];
+		Shifter_Flags = {negative, zero, c_out, overflow};
 	else
 	    if (Instr[4])
-	        regshift regshift(Instr[11:8], Instr[6:5], Rm, Rd);
+	        regshift regshift(Instr[11:8], Instr[6:5], Rm, Rd, ShifterFlags);
 		else
-	        immshift immshift(Instr[11:7], Instr[6:5], Rm, Rd);
+	        immshift immshift(Rs, Instr[6:5], Rm, Rd, ShifterFlags);
 
 end module
