f84f5d49c2d896bdf561cd6483fb405c0f5800a6
Raymond Wang <raymond@insellerate.com>
2017-03-24 20:55:39 -0700

    removed unecessary stuff from dp

--- design/datapath.sv
+++ design/datapath.sv
@@ -18,7 +18,7 @@ module datapath(
 
 
     logic [31:0] PCNext, PCPlus4, PCPlus8, InstrOut;
-    logic [31:0] ExtImm, SrcA, SrcB, Rd, PCPlus8Minus4, Rs, BranchResult;
+    logic [31:0] ExtImm, SrcA, SrcB, Rd, Rs, BranchResult;
 	logic [31:0] Result, MEMResult, EXEResult, IDResult, ResultToPc;
     logic [3:0] RA1, RA2, ShifterFlags, AFlags, BranchFlags;
 	
@@ -29,17 +29,14 @@ module datapath(
     flopr #(32) pcreg(clk, reset, PCNext, PC);
     adder #(32) pcadd1(1'b0, PC, 32'b100, PCPlus4);
     adder #(32) pcadd2(1'b0, PCPlus4, 32'b100, PCPlus8);
-	adder #(32) bladd(InstrIn[24],PCPlus8, 32'b100, PCPlus8Minus4);
-	//IFregfile IFrf(InstrIn, IDResult, InstrOut, ResultToPC);
 
     // register file logic
     mux2 #(4) ra1mux(InstrIn[19:16], 4'b1111, RegSrc[0], RA1);
     mux2 #(4) ra2mux(InstrIn[3:0], InstrIn[15:12], RegSrc[1], RA2);
     regfile rf(clk, RegWrite, RA1, RA2,
-        InstrIn[15:12], InstrIn[11:8], Result, PCPlus8, PCPlus8Minus4,
+        InstrIn[15:12], InstrIn[11:8], Result, PCPlus8, PCPlus4,
         SrcA, WriteData, Rs);		
     extend ext(InstrIn[23:0], ImmSrc, ExtImm);
-	//IDregfile IDrf(SrcA, WriteData, ExtImm, EXEResult, SrcAE, SrcBE, ExtImmE, IDResult);
 
     // ALU logic
 	shifter shifter(InstrIn[25:0], WriteData, Rs, Rd, ShifterFlags);
